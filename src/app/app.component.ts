a9c85a59d5605d54b878642e19d3dfeae8a9def7
vbartalisdev <vbartalisdev@gmail.com>
2021-06-17 16:06:48 +0300

    clear up of template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 import { ElectronService } from './core/services';
-import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   constructor(
     private electronService: ElectronService,
-    private translate: TranslateService
   ) {
-    this.translate.setDefaultLang('en');
     console.log('AppConfig', AppConfig);
 
     if (electronService.isElectron) {
